d0e7c31050768739515b4c0ee632f7d9aa5d095d
samuelp88 <41595779+samuelp88@users.noreply.github.com>
2021-11-28 12:36:40 -0300

    Adicionado comportamento das poções em dispensers

--- src/main/java/com/samuelp88/growth/Growth.java
+++ src/main/java/com/samuelp88/growth/Growth.java
@@ -1,15 +1,26 @@
 package com.samuelp88.growth;
 
+import com.samuelp88.growth.entities.GrowthPotionEntity;
 import com.samuelp88.growth.handlers.RegistryHandler;
+import com.samuelp88.growth.holder.ItemHolder;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.dispenser.IDispenseItemBehavior;
+import net.minecraft.dispenser.IPosition;
+import net.minecraft.dispenser.ProjectileDispenseBehavior;
+import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
+import net.minecraft.entity.projectile.PotionEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Util;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -20,8 +31,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("growth")
@@ -54,6 +63,53 @@ public class Growth {
     private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
         RegistryHandler.registerBrewings();
+        DeferredWorkQueue.runLater(() -> {
+            DispenserBlock.registerBehavior(ItemHolder.GROWTH_POTION_ITEM, new IDispenseItemBehavior() {
+                public ItemStack dispense(IBlockSource p_dispense_1_, ItemStack p_dispense_2_) {
+                    return (new ProjectileDispenseBehavior() {
+                        /**
+                         * Return the projectile entity spawned by this dispense behavior.
+                         */
+                        protected ProjectileEntity getProjectile(World pLevel, IPosition pPosition, ItemStack pStack) {
+                            return Util.make(new GrowthPotionEntity(pLevel, pPosition.x(), pPosition.y(), pPosition.z()), (p_218411_1_) -> {
+                                p_218411_1_.setItem(pStack);
+                            });
+                        }
+
+                        protected float getUncertainty() {
+                            return super.getUncertainty() * 0.5F;
+                        }
+
+                        protected float getPower() {
+                            return super.getPower() * 1.25F;
+                        }
+                    }).dispense(p_dispense_1_, p_dispense_2_);
+                }
+            });
+
+            DispenserBlock.registerBehavior(ItemHolder.STRONG_GROWTH_POTION_ITEM, new IDispenseItemBehavior() {
+                public ItemStack dispense(IBlockSource p_dispense_1_, ItemStack p_dispense_2_) {
+                    return (new ProjectileDispenseBehavior() {
+                        /**
+                         * Return the projectile entity spawned by this dispense behavior.
+                         */
+                        protected ProjectileEntity getProjectile(World pLevel, IPosition pPosition, ItemStack pStack) {
+                            return Util.make(new GrowthPotionEntity(pLevel, pPosition.x(), pPosition.y(), pPosition.z()), (p_218411_1_) -> {
+                                p_218411_1_.setItem(pStack);
+                            });
+                        }
+
+                        protected float getUncertainty() {
+                            return super.getUncertainty() * 0.5F;
+                        }
+
+                        protected float getPower() {
+                            return super.getPower() * 1.25F;
+                        }
+                    }).dispense(p_dispense_1_, p_dispense_2_);
+                }
+            });
+        });
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
